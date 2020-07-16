f1884094255d000cf35d1a5829b9610f2c7b84ff
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 21:23:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		monopoly = {:railroads=> {:pieces => 4, :rent_in_dollars=> {}, :names=> {}  }}
+		monopoly = {:railroads=> {:pieces => 4, :names=> {}, :rent_in_dollars=> {}  }}
 
 end
 
